179d55e621d611383af38bc8dfa91c563eb2b100
Sam Toney <jamstone@ut.utm.edu>
2025-01-23 18:49:25 -0600

    Added findProduct() method to feature branch

--- main.cpp
+++ main.cpp
@@ -16,36 +16,61 @@ using namespace std;
  * Adds numbers between 1 and a user selected number.
  *
  * @param int n User selected integer. 
- * @pre 
+ * @pre User has entered a valid entry.
  * @return int Total sum of numbers between 1 and the user selected number
  * @post The correct sum is properly calculated and returned.
  * 
  */
 int addSum(int n);
 
+
+/**
+ * Finds the product of numbers between 1 and a user selected integer.
+ *
+ * @param int n User selected integer
+ * @pre User has entered a valid entry.
+ * @return int The total product of numbers between 1 and the user selected number.
+ * @post The correct product has been properly calculated and returned.
+ * 
+ */
+int findProduct(int n); 
+
 int main() {
 
   // Initialize variables
-  int n = 0, returnValue = 0;
+  int n = 0, sumValue = 0, productValue = 0;
 
   // Prompt user
   cout << "Please enter an integer: ";
   cin >> n;
 
-  // Return total to main
-  returnValue = addSum(n);
+  // Return totals to main
+  sumValue = addSum(n);
+  productValue = findProduct(n);
 
   // Print total
-  cout << "Total: " << returnValue << endl;
+  cout << "Total sum: " << sumValue << endl;
+  cout << "Total product: " << productValue << endl;
 
   return 0;
 }
 
 int addSum(int n) {
   int total = 0;
-  
+
+  // Find total sum
   for (int i = 1; i <= n; i++) {
     total += i;
+  }
+  return total;
+}
+
+int findProduct(int n) {
+  int total = 1;
+
+  // Find total product
+  for (int i = 1; i <= n; i++) {
+    total *= i;
   }
   return total;
 }
